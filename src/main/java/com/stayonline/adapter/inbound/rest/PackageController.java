b42a31d898fcbcc20a59a79f3864525345d04bcc
Huggo Oliveira <huggo.oliveira@petzpartner.com.br>
2025-09-06 19:27:42 -0300

    feat: ajuste na PakageController

--- src/main/java/com/stayonline/adapter/inbound/rest/PackageController.java
+++ src/main/java/com/stayonline/adapter/inbound/rest/PackageController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.stayonline.application.usecase.ListPackagesUseCase;
 import com.stayonline.domain.model.AccessPackage;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/packages")
 public class PackageController {
+    @Autowired
     private final ListPackagesUseCase listPackages;
 
     public PackageController(ListPackagesUseCase listPackages) {
